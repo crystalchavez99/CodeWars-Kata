8d9cf56b6e79f2b677ef64a814d82c2042c6375b
Crystal <crystal120599@gmail.com>
2023-05-04 10:02:52 -0700

    solved sum of array

---
+++ sumArray.js
@@ -0,0 +1,10 @@
+// Sum Numbers
+function sum (numbers) {
+    "use strict";
+
+    let result = 0;
+    for(let i = 0 ; i < numbers.length; i++){
+      result += numbers[i]
+    }
+  return result;
+};
