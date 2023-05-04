0900ca2cc2411dd71e5f7a7b60fc6c16c086809e
Crystal <crystal120599@gmail.com>
2023-05-04 09:46:12 -0700

    solved opposite numbers

---
+++ oppositeNumber.js
@@ -0,0 +1,11 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+function opposite(number) {
+    //your code here
+    if(number > 0){
+      return -(number)
+    }else if(number < 0){
+      return -(number)
+    }else{
+      return number
+    }
+  }
